355436750e4c6ef208f45ac3e7d3f4a7483f08c9
Gabriel Miranda <gabrielcm.br@gmail.com>
2018-05-19 18:38:50 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,11 +1,20 @@
+<<<<<<< HEAD
 def turn_count(board)
   turn = 0
   board.each do |pos|
     if (pos == "X" || pos == "O")
+=======
+turn = 0
+
+def turn_count(board)
+  board.each do |x|
+    if(x != "" && x != " ")
+>>>>>>> 1a9502cd20d20cc236c631963b4d36eb46106875
       turn += 1
     end
   end
   return turn
+<<<<<<< HEAD
 end
 
 def current_player(board)
@@ -15,4 +24,6 @@ def current_player(board)
   else
     return "X"
   end
+=======
+>>>>>>> 1a9502cd20d20cc236c631963b4d36eb46106875
 end
